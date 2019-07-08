4204a0e25598941150f20334e553850725e69280
Zbigniew Czosnyka <zbigniew.czosnyka@gmail.com>
2019-07-08 11:37:22 +0200

    final commit

--- app/src/main/java/com/example/swoosh3/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh3/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.example.swoosh3.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.swoosh3.Model.Player
@@ -14,9 +15,21 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onMensClicked(view: View){
